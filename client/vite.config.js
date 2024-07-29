a686ac10535abeed8611bdbe10684954b278d61f
Estifanos Bereket <estifanosbereket297@gmail.com>
2024-07-29 10:41:12 +0300

    [] commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,23 +5,8 @@
 // export default defineConfig({
 //   plugins: [react()],
 // });
-// import { defineConfig, loadEnv } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), "");
-//   return {
-//     define: {
-//       "process.env": env,
-//     },
-//     plugins: [react()],
-//   };
-// });
-
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import commonjs from "vite-plugin-commonjs"; // Import the commonjs plugin
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -30,15 +15,30 @@ export default defineConfig(({ mode }) => {
     define: {
       "process.env": env,
     },
-    plugins: [
-      react(),
-      commonjs(), // Add the CommonJS plugin
-    ],
-    build: {
-      sourcemap: true, // Enable source maps for better debugging
-    },
-    optimizeDeps: {
-      include: ["quill"], // Ensure quill is included
-    },
+    plugins: [react()],
   };
 });
+
+// import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react";
+// import commonjs from "vite-plugin-commonjs"; // Import the commonjs plugin
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
+//   return {
+//     define: {
+//       "process.env": env,
+//     },
+//     plugins: [
+//       react(),
+//       commonjs(), // Add the CommonJS plugin
+//     ],
+//     build: {
+//       sourcemap: true, // Enable source maps for better debugging
+//     },
+//     optimizeDeps: {
+//       include: ["quill"], // Ensure quill is included
+//     },
+//   };
+// });
